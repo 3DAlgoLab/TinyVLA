c3f59bd1835259016545c5da37be655bb90db15e
3D ALGO LAB <threealgo@outlook.com>
2024-06-15 23:34:29 +0900

    prepare_mini_data updated

--- vla_pg/prepare_mini_data.py
+++ vla_pg/prepare_mini_data.py
@@ -1,8 +1,26 @@
-# %%
+# %% Check Sentence Piece Model(Tokenizer)
+import sentencepiece as spm
+import os
 from icecream import ic
 import numpy as np
 
-# Language Table Simulation based dataset
+TOKENIZER_PATH = "./paligemma_tokenizer.model"
+if not os.path.exists(TOKENIZER_PATH):
+    print("Downloading the model tokenizer...")
+    os.system(f"gsutil cp gs://big_vision/paligemma_tokenizer.model {TOKENIZER_PATH}")
+    print(f"Tokenizer path: {TOKENIZER_PATH}")
+else:
+    print(f"Tokenizer file: {TOKENIZER_PATH} is already downloaded")
+
+sp = spm.SentencePieceProcessor(TOKENIZER_PATH)  # type: ignore
+
+start_id = 256_000
+reserved_size = 256
+for i in range(start_id, start_id + reserved_size):
+    piece = sp.IdToPiece(i)
+    print(f"id:{i} -->piece:{piece}")
+
+# %% Language Table Simulation based dataset
 START_ID = 0
 MAX_STEP_RANGE = 60  # mm (-30 ~ + 30)
 
