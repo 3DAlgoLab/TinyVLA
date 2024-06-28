78663d7f53b54039b67db8241215bb55dc3aa30f
3D ALGO LAB <threealgo@outlook.com>
2024-06-28 20:12:54 +0900

    move jax_n_flax to study repo.

---
+++ paligemma_based/hf/finetune_trial.py
@@ -0,0 +1,122 @@
+# %%
+import pwd
+import torch
+from datasets import load_dataset
+from peft import LoraConfig, get_peft_model
+from transformers import (
+    BitsAndBytesConfig,
+    PaliGemmaForConditionalGeneration,
+    PaliGemmaProcessor,
+    Trainer,
+    TrainingArguments,
+)
+
+ds = load_dataset("HuggingFaceM4/VQAv2", split="train")
+cols_remove = ["question_type", "answers", "answer_type", "image_id", "question_id"]
+ds = ds.remove_columns(cols_remove)
+ds = ds.train_test_split(test_size=0.1)  # type: ignore
+train_ds = ds["train"]
+val_ds = ds["test"]
+
+
+model_id = "google/paligemma-3b-pt-224"
+
+# %%
+
+
+processor = PaliGemmaProcessor.from_pretrained(model_id)
+image_token = processor.tokenizer.convert_tokens_to_ids("<image>")
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def collate_fn(examples):
+    texts = ["answer " + example["question"] for example in examples]
+    labels = [example["multiple_choice_answer"] for example in examples]
+    images = [example["image"].convert("RGB") for example in examples]
+    tokens = processor(
+        text=texts, images=images, suffix=labels, return_tensors="pt", padding="longest"
+    )  # type: ignore
+
+    tokens = tokens.to(torch.bfloat16)
+    return tokens
+
+
+# model = PaliGemmaForConditionalGeneration.from_pretrained(
+#     model_id, torch_dtype=torch.bfloat16
+# ).to(device)
+
+model = PaliGemmaForConditionalGeneration.from_pretrained(
+    model_id, torch_dtype=torch.bfloat16, device_map="auto"
+)
+
+
+for param in model.vision_tower.parameters():
+    param.requires_grad = False
+
+for param in model.multi_modal_projector.parameters():
+    param.requires_grad = True
+
+print(model)
+
+# %%
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_type=torch.bfloat16
+)
+
+lora_config = LoraConfig(
+    r=4,
+    target_modules=[
+        "q_proj",
+        "o_proj",
+        "k_proj",
+        "v_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+    ],
+    task_type="CAUSAL_LM",
+)
+
+
+model = PaliGemmaForConditionalGeneration.from_pretrained(
+    model_id, quantization_config=bnb_config, device_map="auto"
+)
+model = get_peft_model(model, lora_config)
+model.print_trainable_parameters()
+
+# %%
+# %pwd
+
+# %%
+args = TrainingArguments(
+    num_train_epochs=2,
+    remove_unused_columns=False,
+    per_device_train_batch_size=16,
+    gradient_accumulation_steps=4,
+    warmup_steps=2,
+    learning_rate=2e-5,
+    weight_decay=1e-6,
+    adam_beta2=0.999,
+    logging_steps=100,
+    optim="adamw_hf",
+    save_strategy="steps",
+    save_steps=1000,
+    push_to_hub=True,
+    save_total_limit=1,
+    bf16=True,
+    report_to=["tensorboard"],
+    dataloader_pin_memory=False,
+    output_dir="./tmp",
+)  # type: ignore
+
+trainer = Trainer(  # type: ignore
+    model=model,
+    train_dataset=train_ds,
+    eval_dataset=val_ds,
+    data_collator=collate_fn,
+    args=args,
+)
+
+trainer.train()
+
+# %%
